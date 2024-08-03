69486e620d68ae5f09e9bacb380c3ed18ecc616f
TonyKun <tal96047@gmail.com>
2024-08-03 13:15:02 +0300

    done it for now until i remember one more thing

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const port = process.env.PORT || 3050;
 
 server.use(cors()); 
 server.use(express.json());
-//middleware.js
+
+//./Modules/middleware
 server.use(Logs);
-//Products.js
+
+//./Modules/products
 server.get('/', Welcome);
 server.get('/products', checkItems);
 server.get('/products/:id', CheckItemByID);
@@ -19,12 +21,13 @@ server.post('/products', AddItem);
 server.put('/products/:id', UpdateItem);
 server.delete('/products/:id', DeleteItem);
 
-//Category.js
+//./Modules/Category
 server.get('/category', checkCategory);
 server.post('/category', AddCategory);
 server.put('/category/:name', UpdateCategory);
 server.delete('/category/:name', DeleteCategory);
-//Login.js
+
+//./Modules/Login
 server.post('/Login/checkUsers',checkUsers)
 server.post('/Login/CreateUser',CreateUser);
 server.post('/Login/:token',CheckByToken);
